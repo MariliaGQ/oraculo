3c425057836537740ce11a1f7855820abbf77c47
Marilia Gabriela de Quadros <mariliag.dequadros@gmail.com>
2025-04-21 21:15:36 -0300

    feat: Implementar sidebar para seleção de arquivos e modelos

--- src/chat.py
+++ src/chat.py
@@ -6,6 +6,17 @@ MENSAGENS_EXEMPLO = [
     ('user', 'tudo certo')
 ]
 
+CONFIG_MODELOS =  {'Groq': {'modelos':['gemma2-9b-it', 'llama-3.3-70b-versatile']},
+                    'OpenAI': {'modelos': ['gpt-4o-mini', 'gpt-4o']}}
+
+TIPOS_ARQUIVOS_VALIDOS = [
+    'Site',
+    'Youtube',
+    'PDF',
+    'CSV',
+    'TXT'
+]
+
 
 def pagina_chat():
     st.header('🤖 Bem-vindo ao Oráculo', divider = True)
@@ -22,14 +33,34 @@ def pagina_chat():
         mensagens.append(('user', input_usuario))
         st.session_state['mensagens'] = mensagens
         st.rerun()
-       
 
-        
+def sidebar():
+    tabs = st.sidebar.tabs(['Upload Arquivos', 'Seleção de Modelos'])  
+    with tabs[0]:
+        tipo_arquivo = st.selectbox("Selecione o tipo de arquivo", TIPOS_ARQUIVOS_VALIDOS) 
+        if tipo_arquivo == 'Site':
+            arquivo = st.text_input("Cole o link do site aqui")
+        if tipo_arquivo == 'Youtube':
+            arquivo = st.text_input("Cole o link do vídeo aqui")           
+        if tipo_arquivo == 'PDF':
+            arquivo = st.file_uploader("Envie o arquivo PDF aqui", type='pdf')
+        if tipo_arquivo == 'CSV':
+            arquivo = st.file_uploader("Envie o arquivo CSV aqui", type='csv')
+        if tipo_arquivo == 'TXT':
+            arquivo = st.file_uploader("Envie o arquivo TXT aqui", type='txt')
+    with tabs[1]:
+        provedor = st.selectbox("Selecione o provedor", CONFIG_MODELOS.keys())
+        modelo = st.selectbox("Selecione o modelo", CONFIG_MODELOS[provedor]['modelos'])
+        api_key = st.text_input("Cole sua chave de API aqui", value=st.session_state.get(f'api_key_{provedor}', ''))
+
+        st.session_state[f'api_key_{provedor}'] = api_key
      
 
 
 def main():
     pagina_chat()
+    with st.sidebar:
+        sidebar()
 
 
 if __name__ == '__main__':
